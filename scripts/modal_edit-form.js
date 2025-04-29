d79776cdce804d08daceb3a8aaf4a081c331ee07
raymondfrias <rfrias361@gmail.com>
2025-04-29 14:08:01 -0400

    make edit profile form work correctly when clicking the save button, and the page does not reload when saving

--- scripts/modal_edit-form.js
+++ scripts/modal_edit-form.js
@@ -1,6 +1,24 @@
 import { openModal , closeModal } from './global_utils.js';
 const editProfileBtn = document.querySelector('#profile__edit');
+const editSaveBtn = document.querySelector('#profile__save');
+const profileNameTitle = document.querySelector('#profile_name_title');
+const editNameInput = document.querySelector('#profile-name');
+const profileDescriptionTitle = document.querySelector('#profile__profession');
+const editDescriptionInput = document.querySelector('#profile-description');
 const modalContainer = document.querySelector('#modal__edit');
 const modalCloseBtn = document.querySelector('#modal__exit-edit');
 openModal(editProfileBtn , modalContainer);
 closeModal(modalCloseBtn , modalContainer);
+editNameInput.value = profileNameTitle.textContent;
+editDescriptionInput.value = profileDescriptionTitle.textContent;
+editNameInput.placeholder = profileNameTitle.textContent;
+editDescriptionInput.placeholder = profileDescriptionTitle.textContent;
+editSaveBtn.addEventListener('click' , (e) => {
+    e.preventDefault()
+    profileNameTitle.textContent = editNameInput.value;
+    editNameInput.value = profileNameTitle.textContent;
+    profileDescriptionTitle.textContent = editDescriptionInput.value;
+    editDescriptionInput.value = profileDescriptionTitle.textContent;
+    modalContainer.classList.remove('modal__opened');
+    modalContainer.classList.add('modal__hidden');
+})
